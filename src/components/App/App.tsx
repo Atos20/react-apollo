9ca4e38e848ef606792068028043d4817402eff4
Orlando Murcio <ato.murcio@gmail.com>
2021-02-17 13:51:36 -0700

    Remove yarn file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { React } from 'react'
 import './App.css'
+import Header from '../Header/Header';
+import { Switch } from 'react-router-dom';
 
 const App: React.FC = () => {
+
   return (
     <div>
-      
+      <Header />
+      <Switch>
+        
+      </Switch>
     </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,22 +2,27 @@ import React from 'react'
 import './Header.css'
 import { FiDroplet } from "react-icons/fi";
 import { NavLink } from 'react-router-dom'
+
 const Header = () => {
   return (
     <header className='head'>
       <nav className="nav">
-        <ul >
+        <ul className='unordered'>
+          <li>
+           <FiDroplet />
+            <NavLink className='nav-link' to='/'>Home</NavLink>
+          </li>
           <li >
-            <NavLink to='/'><FiDroplet/>Home</NavLink>
+            <FiDroplet/>
+            <NavLink className='nav-link' to='/about'>About</NavLink>
           </li>
           <li>
-            <NavLink to='/about'><FiDroplet/>About</NavLink>
+            <FiDroplet/>
+            <NavLink className='nav-link'to='/location'>Location</NavLink>
           </li>
           <li>
-            <NavLink to='/location'><FiDroplet/>Location</NavLink>
-          </li>
-          <li>
-            <NavLink to='/contact'><FiDroplet/>Contact me!</NavLink>
+          <FiDroplet/>
+            <NavLink className='nav-link'to='/contact'>Contact me!</NavLink>
           </li>
         </ul>
       </nav>
