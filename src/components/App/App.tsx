5a1dbcb3a02f96f1f15e07e619d1f92715054842
Orlando Murcio <ato.murcio@gmail.com>
2021-02-17 14:11:09 -0700

    Add functionality to nav links

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,31 @@
+//libraries
 import { React } from 'react'
-import './App.css'
+import { Switch, Route } from 'react-router-dom';
+//pages/compoenents
 import Header from '../Header/Header';
-import { Switch } from 'react-router-dom';
+import { Home } from '../../Pages/Home/Home'
+import { About } from '../../Pages/About/About'
+//styles
+import './App.css'
 
 const App: React.FC = () => {
 
   return (
-    <div>
+    <>
       <Header />
+
       <Switch>
         
+        <Route path='/about'>
+          <About/>
+        </Route>
+        <Route path='/'>
+          <Home/>
+        </Route>
+
+
       </Switch>
-    </div>
+    </>
   )
 }
 
