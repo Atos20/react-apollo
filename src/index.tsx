aeaa8894c4ce4a436c6b948a843765a891ed13cb
Orlando Murcio <ato.murcio@gmail.com>
2021-02-17 16:38:37 -0700

    Wrapp Apollo around App

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,19 @@ import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { client } from './common/apollo-client'
+import  { ApolloProvider } from '@apollo/client'
+//styles
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </ApolloProvider>
+  ,
   document.getElementById('root')
 );
 
